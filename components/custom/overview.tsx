9bbad05b9db5bfa58bac020111f26f9cdcf78c8b
jamilsonokay <125391717+jamilsonokay@users.noreply.github.com>
2025-05-10 10:26:00 -0100

    Update overview.tsx
    Customizações

--- components/custom/overview.tsx
+++ components/custom/overview.tsx
@@ -15,8 +15,8 @@ export const Overview = () => {
     >
       <div className="border-none bg-muted/50 rounded-2xl p-6 flex flex-col gap-4 text-zinc-500 text-sm dark:text-zinc-400 dark:border-zinc-700">
         <p className="flex flex-row justify-center gap-4 items-center text-zinc-900 dark:text-zinc-50">
-          Eu sou o I am Chabot!
-          Em que posso ajudar? 
+          Olá,sou o i'm Chabot!
+          Como posso te ajudar hoje? 
         </p>
       </div>
     </motion.div>
